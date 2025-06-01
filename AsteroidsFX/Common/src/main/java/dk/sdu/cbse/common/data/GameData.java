04a87a5e6bcd6b4a746ae014327bce145d6a4381
Lucasbirkc <Lucasbirkc@gmail.com>
2025-06-01 22:37:59 +0200

    Added modules for GameLab

---
+++ AsteroidsFX/Common/src/main/java/dk/sdu/cbse/common/data/GameData.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dk.sdu.cbse.common.data;
+
+/**
+ *
+ * @author Lucas
+ */
+public class GameData {
+    private int displayWidth  = 800 ;
+    private int displayHeight = 800;
+    private final GameKeys keys = new GameKeys();
+
+    public GameKeys getKeys() {
+        return keys;
+    }
+
+    public void setDisplayWidth(int width) {
+        this.displayWidth = width;
+    }
+
+    public int getDisplayWidth() {
+        return displayWidth;
+    }
+
+    public void setDisplayHeight(int height) {
+        this.displayHeight = height;
+    }
+
+    public int getDisplayHeight() {
+        return displayHeight;
+    }
+
+}
